384254acec0da942c02729f3f046008868d31917
Graham Long <longevity_software@mail.com>
2020-12-30 15:41:44 +0000

    Changed some settings, MetaData etc

--- LongevitySoftware/gatsby-config.js
+++ LongevitySoftware/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Longevity Software Solutions`,
+    description: `Longevity Software website.`,
+    author: `Graham Long`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,10 +18,10 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `longevity-software-site`,
+        short_name: `longevity-software`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#f7fdf7`,
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
--- LongevitySoftware/src/components/layout.js
+++ LongevitySoftware/src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header siteTitle={data.site.siteMetadata ?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
@@ -37,9 +37,7 @@ const Layout = ({ children }) => {
         <footer style={{
           marginTop: `2rem`
         }}>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          © {new Date().getFullYear()} Longevity Software Solutions
         </footer>
       </div>
     </>
